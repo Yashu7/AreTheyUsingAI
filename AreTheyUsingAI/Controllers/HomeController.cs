b7fe780ae31c7cf7aeae28f02c557340921a6586
Yashu7 <janmalawski2@gmail.com>
2024-12-12 23:27:47 +0100

    Local connection string, sqlreader

--- AreTheyUsingAI/Controllers/HomeController.cs
+++ AreTheyUsingAI/Controllers/HomeController.cs
@@ -1,16 +1,48 @@
-using AreTheyUsingAI.Models;
+﻿using AreTheyUsingAI.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace AreTheyUsingAI.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly string _connectionString;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _connectionString = configuration.GetConnectionString("AreTheyUsingAILocalDB");
+            ExecuteQuery();
+        }
+        public void ExecuteQuery()
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                using (var command = new SqlCommand("SELECT * FROM POST", connection))
+                {
+                    command.CommandType = CommandType.Text;
+                    // Add parameters if needed
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        int id = 0;
+                        string text = "";
+                        while (reader.Read())
+                        {
+                            text = reader["PostTitle"].ToString();
+                        }
+                    }
+                }
+            }
         }
 
         public IActionResult Index()
--- AreTheyUsingAI/Program.cs
+++ AreTheyUsingAI/Program.cs
@@ -1,32 +1,26 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
 namespace AreTheyUsingAI
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
-
-            app.Run();
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
